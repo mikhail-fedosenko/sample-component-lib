fa9850d046bbc3383be2fa1f902cf93c01902158
Mikhail Fiadosenka <mfiadosenka@objectstyle.com>
2022-10-28 13:56:01 +0300

    first build to /dist

---
+++ dist/components/Button/Button.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+var Button = function (props) {
+    return _jsx("button", { children: props.label });
+};
+export default Button;
+//# sourceMappingURL=Button.js.map
