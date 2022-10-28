c5455a1139e78a889c87681aedcf158e14956b82
Mikhail Fiadosenka <mfiadosenka@objectstyle.com>
2022-10-28 14:05:29 +0300

    add Button.css and build

--- dist/components/Button/Button.js
+++ dist/components/Button/Button.js
@@ -1,6 +1,18 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 import { jsx as _jsx } from "react/jsx-runtime";
+import "./Button.css";
 var Button = function (props) {
-    return _jsx("button", { children: props.label });
+    return _jsx("button", __assign({ className: "custom-button" }, { children: props.label }));
 };
 export default Button;
 //# sourceMappingURL=Button.js.map
